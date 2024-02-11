88b751f4367965c3b7b7e729fb6ef49fa335333c
Junior Freitas <joaofbjunior01@gmail.com>
2024-02-10 22:46:25 -0300

    fix: add ssl temp

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,26 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { validationPipeOptions } from './config/validation';
+import { readFileSync } from 'fs';
+import pem from 'pem';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const promise: Promise<pem.CertificateCreationResult> = new Promise((resolve, reject) => {
+    pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+      if (err) {
+        reject(err);
+      }
+      resolve(keys);
+    });
+  });
+  const keys = await promise;
+  const httpsOptions = {
+    key: keys.clientKey,
+    cert: keys.certificate,
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Unidos no Senhor API')
@@ -17,6 +34,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
+  console.log(`EAI: ${process.env}`);
   await app.listen(3003);
 }
 bootstrap();
