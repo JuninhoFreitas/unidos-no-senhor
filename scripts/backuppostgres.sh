6de10ed9bc201ecd1ec114fee85ba5cfa9fb8068
Junior Freitas <joaofbjunior01@gmail.com>
2024-02-11 22:44:53 -0300

    build: add script to backup database data to a s3

---
+++ scripts/backuppostgres.sh
@@ -0,0 +1,31 @@
+# Script to Backup PostgreSQL Database to a file based in docker container
+# Database: unidos
+# Tables: membro, biblioteca
+
+# Variables
+#lOAD ENVIRONMENT VARIABLES
+DATE=$(date +"%Y-%m-%d")
+TIME=$(date +"%H-%M-%S")
+BACKUP_DIR="../backup"
+CONTAINER_NAME="postgresdb"
+DATABASE_NAME=""
+DATABASE_USER=""
+DATABASE_PASSWORD=""
+BUCKET=""
+FILE_NAME="./$DATABASE_NAME-$DATE-$TIME.sql"
+
+# Create backup directory
+
+if [ ! -d $BACKUP_DIR ]; then
+  mkdir -p $BACKUP_DIR
+fi
+
+# Backup database to file and compress it in a pipe 
+# must authenticate with password
+docker exec -i ${CONTAINER_NAME} /bin/bash -c "PGPASSWORD=${DATABASE_PASSWORD} pg_dump --username ${DATABASE_USER} ${DATABASE_NAME}" > $FILE_NAME
+
+# Send to a s3 bucket
+aws s3 cp $FILE_NAME $BUCKET
+
+# Restore database from file
+# docker exec -i ${CONTAINER_NAME}  /bin/bash -c "PGPASSWORD=${DATABASE_PASSWORD} psql --username ${DATABASE_USER} ${DATABASE_NAME}" < ./dump.sql
